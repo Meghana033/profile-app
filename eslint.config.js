7cc9de8a4bd6ec4c36a42f83ad15bfdac0a5ecaa
Meghana033 <meghanamadireddy7@gmail.com>
2026-05-29 21:55:41 +0530

    eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,21 +1,68 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+
+import globals from "globals";
+
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
+import {
+  defineConfig,
+  globalIgnores
+} from "eslint/config";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+
+  // Ignore Build Folder
+  globalIgnores([
+    "dist",
+    "node_modules"
+  ]),
+
   {
-    files: ['**/*.{js,jsx}'],
-    extends: [
-      js.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
+    files: [
+      "**/*.{js,jsx}"
     ],
+
+    extends: [
+
+      js.configs.recommended,
+
+      reactHooks.configs.flat.recommended,
+
+      reactRefresh.configs.vite
+    ],
+
     languageOptions: {
-      globals: globals.browser,
-      parserOptions: { ecmaFeatures: { jsx: true } },
+
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      },
+
+      parserOptions: {
+
+        ecmaVersion: "latest",
+
+        sourceType: "module",
+
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
     },
-  },
-])
+
+    rules: {
+
+      // Disable unused vars warning
+      "no-unused-vars": "off",
+
+      // React Refresh Rule
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true
+        }
+      ]
+    }
+  }
+]);
